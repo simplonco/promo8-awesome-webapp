a462d5d3bed02233e00a23ef7f4d055103943bff
ShamShams <shamila.remtoula@hotmail.fr>
2017-08-28 14:57:57 +0200

    ajouter un apprenant

--- app/routes/learners.js
+++ app/routes/learners.js
@@ -4,11 +4,22 @@ const Learner = require('../models/learner');
 
 router.get('/list', (request, response) =>{
   Learner.find((error, learners) =>{
-    console.log(learners);
-      response.render('learners/list', { learners });
-  })
-
+    if (error) {
+      response.send(error);
+    }
+    response.render('learners/list', { learners });
+  });
 });
 
+router.get('/add', (request, response) =>{
+    response.render('learners/add');
+});
+
+router.post('/add', (request, response) =>{
+    const learner = new Learner(request.body);
+    learner.save((error) => {
+      response.redirect('/list');
+    });
+});
 
 module.exports = router;
