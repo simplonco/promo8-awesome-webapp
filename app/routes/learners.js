4690fbe8f06283ce1a22da94838b7ee3ce26004c
ShamShams <shamila.remtoula@hotmail.fr>
2017-08-28 17:08:06 +0200

    air-bnb-base installed

--- app/routes/learners.js
+++ app/routes/learners.js
@@ -1,9 +1,10 @@
 const express = require('express');
+
 const router = express.Router();
 const Learner = require('../models/learner');
 
-router.get('/list', (request, response) =>{
-  Learner.find((error, learners) =>{
+router.get('/list', (request, response) => {
+  Learner.find((error, learners) => {
     if (error) {
       response.send(error);
     }
@@ -11,36 +12,38 @@ router.get('/list', (request, response) =>{
   });
 });
 
-router.get('/add', (request, response) =>{
-    response.render('learners/add');
+router.get('/add', (request, response) => {
+  response.render('learners/add');
 });
 
-router.post('/add', (request, response) =>{
-    const learner = new Learner(request.body);
-    learner.save((error) => {
-      response.redirect('/list');
-    });
+router.post('/add', (request, response) => {
+  const learner = new Learner(request.body);
+  learner.save((error) => {
+    if (error) {
+      response.send(error);
+    }
+    response.redirect('/list');
+  });
 });
 
 router.post('/edit/:id', (request, response) => {
-  Learner.findByIdAndUpdate(request.params.id, request.body, (error)=>{
-    if(error) response.send(error);
+  Learner.findByIdAndUpdate(request.params.id, request.body, (error) => {
+    if (error) response.send(error);
     response.redirect('/list');
   });
 });
 
 router.get('/remove/:id', (request, response) => {
   Learner.findByIdAndRemove(request.params.id, (error) => {
-    if(error) response.send(error);
+    if (error) response.send(error);
     response.redirect('/list');
   });
 });
 
 router.get('/edit/:id', (request, response) => {
-  console.log(request.params);
-  Learner.findById(request.params.id, (error, learner) =>{
-    if(error)  response.send(error);
-    response.render("learners/edit", { learner });
+  Learner.findById(request.params.id, (error, learner) => {
+    if (error) response.send(error);
+    response.render('learners/edit', { learner });
   });
 });
 
